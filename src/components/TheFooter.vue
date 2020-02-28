2d5065448eb9e3e71a3b22310de993fa6d08d278
Naison Pontes <npontes@Mac-mini-de-Naison.local>
2020-02-28 12:33:56 -0300

    base project

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer>Alguns direitos reservados.</footer>
+</template>
+
+<script>
+export default {
+  name: "TheFooter"
+};
+</script>
+
+<style>
+footer {
+  background: #87f;
+  padding: 25px 0;
+  text-align: center;
+  font-size: 1.2rem;
+  color: #fff;
+  position: relative;
+  top: 75px;
+}
+</style>
